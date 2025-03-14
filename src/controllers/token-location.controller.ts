6889559b7c8edec2a6951c0a3eeaa2e8cc0127d1
Adam Smith <adam.smith@similie.org>
2025-03-14 17:17:00 +0900

    chore: removed logging

--- src/controllers/token-location.controller.ts
+++ src/controllers/token-location.controller.ts
@@ -72,7 +72,6 @@ export default class TokenController extends EllipsiesController<TokenLocation> 
     @Req() req: ExpressRequest,
   ): Promise<TokenLocation[]> {
     const token: string | undefined = req.query.token as string | undefined;
-    console.log("I AM THIS TOKEN AND I DESERVER MORE", token);
     if (!token) {
       return [];
     }
@@ -81,20 +80,17 @@ export default class TokenController extends EllipsiesController<TokenLocation> 
         populate: ["user"],
       });
       const found = await agentToken.findOneBy({ token });
-      console.log("WHAT AND I FOUND", found);
       if (!found || !found.user) {
         return [];
       }
 
       const user = typeof found.user === "string" ? found.user : found.user.id;
-      console.log("I AM THE USER", user);
       const queryAgent = new QueryAgent<TokenLocation>(TokenLocation, {
         populate: ["*"],
         sort: { createdAt: "desc" },
         where: { user },
       });
       const values = (await queryAgent.getObjects()) as TokenLocation[];
-      console.log("FOUND THESE VALUES", values);
       return this.findAlerts(values) as unknown as TokenLocation[];
     } catch (e) {
       console.error("ERROR", e);
